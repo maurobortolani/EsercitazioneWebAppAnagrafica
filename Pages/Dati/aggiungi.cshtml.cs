293e99a26f257d138500b5617aa44f192a1cb220
maurobortolani <mauro.bortolani@ialedu.it>
2024-03-11 08:51:06 +0100

    settore Dati finito

---
+++ Pages/Dati/aggiungi.cshtml.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static WebApplication1.Pages.Dati.listaDatiModel;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace WebApplication1.Pages.Dati
+{
+    public class aggiungiModel : PageModel
+    {
+		public string errorMessage = "";
+		public dati dato = new dati();
+
+		public void OnGet()
+		{
+
+		}
+
+		public void OnPost()
+		{
+			dato.Data = Request.Form["data"];
+			dato.Valore = Request.Form["valore"];
+
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(configurazioni.connectionString))
+				{
+					connection.Open();
+					DateTime dt = DateTime.ParseExact(dato.Data, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+					string sql = $"INSERT INTO Dati " +
+						$"(Data, Valore) VALUES " +
+						$"('{dt.ToString("MM/dd/yyyy HH:mm")}', '{dato.Valore.Trim()}')";
+
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						command.ExecuteNonQuery();
+					}
+					connection.Close();
+				}
+			}
+			catch (Exception ex)
+			{
+				errorMessage = ex.Message;
+			}
+
+			Response.Redirect("listaDati");
+		}
+	}
+}
